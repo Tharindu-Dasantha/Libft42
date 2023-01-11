6ae51a0aa77ce866dcef3c807638bc637b6ac332
SAMHRACHI <gyulumyansamvel1@gmail.com>
2023-01-11 14:47:53 +0400

    hardwork

--- functions/ft_atoi.c
+++ functions/ft_atoi.c
@@ -8,8 +8,6 @@
 /* |        ##     ## ## ##      This is libft                | */
 /* |----------------------------------------------------------| */
 
-// #include <stdio.h>
-// #include <string.h>
 #include "libft.h"
 int ft_atoi(const char *str)
 {
@@ -29,8 +27,8 @@ int ft_atoi(const char *str)
      res = res * 10 + (*str++ - '0');
     return res*sign;
 }
-int main(int argc, char *argv[])
-{
-    printf("mine:%d\n",ft_atoi(argv[1]));
-    return 0;
-}
+// int main(int argc, char *argv[])
+// {
+//     printf("mine:%d\n",ft_atoi(argv[1]));
+//     return 0;
+// }
